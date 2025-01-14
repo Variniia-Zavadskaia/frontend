21f22496c5513d11268a548e2531817bd42a3c30
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-14 23:29:34 +0200

    UserName cmp

---
+++ src/cmps/elements/UserName.jsx
@@ -0,0 +1,16 @@
+import { useNavigate } from 'react-router'
+
+export function UserName({user, isLink = true}) {
+    const navigate = useNavigate()
+ 
+    function onUserNameClick() {
+        if (isLink) {
+            navigate(`/user/${user._id}`)
+        }
+    }    
+    
+    // console.log(user); 
+    
+
+    return <span className="user-name" onClick={onUserNameClick} >{user.fullname}</span>
+}
