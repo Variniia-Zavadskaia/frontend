04bac6187086a0037ad23597b9961a74be8b6f43
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-15 22:10:08 +0200

    add size input to usericon

--- src/cmps/elements/UserIcon.jsx
+++ src/cmps/elements/UserIcon.jsx
@@ -1,18 +1,22 @@
 // import { Link } from 'react-router-dom'
 import { useNavigate } from 'react-router'
 
-export function UserIcon({user, isLink = true}) {
+export function UserIcon({ user, size=60, isLink = true }) {
     const navigate = useNavigate()
- 
+
+    console.log(size);
+
+
     function onIconClick() {
         if (isLink) {
             navigate(`/user/${user._id}`)
         }
-    }    
-    
+    }
+
     return (
-        <div className="user-icon" onClick={onIconClick}>
-            {user.imgUrl && <img src={user.imgUrl} className="user-img" />}
+        <div className="user-icon" onClick={onIconClick} style={{ width: `${size}px`, height: `${size}px`}}>
+            {user.imgUrl && 
+                <img src={user.imgUrl} className="user-img" />}
         </div>
     )
 }
