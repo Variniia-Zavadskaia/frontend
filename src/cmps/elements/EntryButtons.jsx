a0a54bdf4e9847eaf493669442fc058710f54d13
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-16 21:01:14 +0200

    Add EntryButtons cmp

---
+++ src/cmps/elements/EntryButtons.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { entrySvg } from '../Svgs'
+
+export function EntryButtons({ entry }) {
+    //TODO btn comment -> open entry modal
+    //TODO btn share -> open modal with following
+
+    const [likes, setLikes] = useState(entry.likes || 0)
+    const [isToggled, setIsToggled] = useState(false)
+
+    const handleLike = () => {
+        setIsToggled(!isToggled)
+        setLikes(!isToggled ? likes + 1 : likes - 1)
+    }
+
+    return (
+        <section>
+            <div className="actions">
+                <div>
+                    <div onClick={handleLike} style={{ cursor: 'pointer' }}>
+                        {isToggled ? (
+                            <button className="action like">{entrySvg.fullHeart}</button>
+                        ) : (
+                            <button className="action like">{entrySvg.heart}</button>
+                        )}
+                    </div>
+                    <button className="action comment">{entrySvg.comment}</button>
+                    <button className="action share">{entrySvg.share}</button>
+                </div>
+                <button className="action save">{entrySvg.save}</button>
+            </div>
+
+            <div className="entry-likes">{likes} likes</div>
+        </section>
+    )
+}
