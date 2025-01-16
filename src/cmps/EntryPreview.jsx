0622503e1cbc9bee6dff8c3f7a9bb94421b6b1f4
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-16 21:01:58 +0200

    Use EntryButtons in EntryPreview

--- src/cmps/EntryPreview.jsx
+++ src/cmps/EntryPreview.jsx
@@ -7,7 +7,7 @@ import { onToggleModal } from '../store/actions/app.actions'
 import React, { useState } from 'react'
 import { UserIcon } from './elements/UserIcon'
 import { UserName } from './elements/UserName'
-// import { LikeButtom } from './elements/LikeButton'
+import { EntryButtons } from './elements/EntryButtons'
 
 export function EntryPreview({ entry, onRemoveEntry, onUpdateEntry }) {
     // const user = useSelector(storeState => storeState.userModule.user)
@@ -24,22 +24,18 @@ export function EntryPreview({ entry, onRemoveEntry, onUpdateEntry }) {
     }
 
     const [likes, setLikes] = useState(entry.likes || 0)
-    const [liked, setLiked] = useState(false)
+    const [isToggled, setIsToggled] = useState(false)
 
     const handleLike = () => {
-        setLiked(!liked)
-        setLikes(liked ? likes - 1 : likes + 1)
+        setIsToggled(!isToggled)
+        setLikes(!isToggled ? likes + 1 : likes - 1)
     }
-    const heartIcon = React.cloneElement(entrySvg.heart, {
-        fill: liked ? 'red' : 'black',
-        // stroke: liked ? 'red' : 'black',
-    })
 
     return (
         <article className="preview">
             <header>
                 <div className="prof-preview">
-                    <UserIcon user={userBy} size={32}/>
+                    <UserIcon user={userBy} size={32} />
                     <UserName calssName="text first" user={userBy} />
                 </div>
                 <button onClick={onOptions}>{entrySvg.option}</button>
@@ -52,23 +48,14 @@ export function EntryPreview({ entry, onRemoveEntry, onUpdateEntry }) {
 
             {/* {entry.owner && <p>Owner: <span>{entry.owner.fullname}</span></p>} */}
             <div className="entry-info">
-                <div className="actions">
-                    <button className="action like" onClick={handleLike}>
-                        {heartIcon}
-                    </button>
-                    <button className="action comment">{entrySvg.comment}</button>
-                    <button className="action share">{entrySvg.share}</button>
-                    <button className="action save">{entrySvg.save}</button>
-                </div>
-
-                <div className="entry-likes">{likes} Likes</div>
+                <EntryButtons entry={entry} />
                 <div className="entry-comment">
                     <p>
                         <NavLink to={`/user/${userBy._id}`}>{userBy.fullname}</NavLink> {entry.txt}
                     </p>
                 </div>
 
-                <span className="view-comments">View all 20 comments</span>
+                {/* <span className="view-comments">View all 20 comments</span> */}
                 <div className="entry-comments">
                     <textarea name="txt" placeholder="Add a comment..."></textarea>
                     <button>{entrySvg.emoji}</button>
