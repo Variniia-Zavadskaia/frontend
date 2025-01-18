fcd11dc5f13b1c0fc91046e3331c9b615ea7df3f
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-18 18:39:09 +0200

    EntryButtons: fix likes print and clear code

--- src/cmps/elements/EntryButtons.jsx
+++ src/cmps/elements/EntryButtons.jsx
@@ -4,14 +4,13 @@ import { entrySvg } from '../Svgs'
 import { addEntryLike, removeEntryLike } from '../../store/actions/entry.actions'
 import { LikeButton } from './LikeButton'
 
-export function EntryButtons({ entry, onUpdateEntry}) {
+export function EntryButtons({ entry }) {
     //TODO btn comment -> open entry modal
     //TODO btn share -> open modal with following
 
     const { _id, fullname, imgUrl } = useSelector(storeState => storeState.userModule.user)
-    // const [likesCount, setLikesCount] = useState(entry.likedBy.length)
     const [isLiked, setIsLiked] = useState(false)
-    const [likedBy, setLikedBy] = useState([...entry.likedBy]);
+    const [likedBy, setLikedBy] = useState([...entry.likedBy])
 
     useEffect(() => {
         setIsLiked(entry.likedBy.some(likedUser => likedUser._id === _id))
@@ -19,36 +18,36 @@ export function EntryButtons({ entry, onUpdateEntry}) {
 
     function handleLike() {
         setIsLiked(!isLiked)
-        // likesCount(!isLiked ? likesCount + 1 : likesCount - 1)
 
-        let updatedLikedBy;
+        let updatedLikedBy
         if (isLiked) {
             // Delete like
-            updatedLikedBy = likedBy.filter((likedUser) => likedUser._id !== _id);
+            updatedLikedBy = likedBy.filter(likedUser => likedUser._id !== _id)
             removeEntryLike(entry._id, _id)
         } else {
             // Add like
-            updatedLikedBy = [...likedBy, { _id, fullname, imgUrl }];
+            updatedLikedBy = [...likedBy, { _id, fullname, imgUrl }]
             addEntryLike(entry._id, { _id, fullname, imgUrl })
-
         }
-        setLikedBy([...updatedLikedBy]);
-        
-        // onUpdateEntry({...entry, likedBy: updatedLikedBy})
+        setLikedBy([...updatedLikedBy])
     }
 
     return (
         <section>
             <div className="entry-buttons">
                 <div className="like-share">
-                    <LikeButton isLiked={isLiked} handleLike={handleLike}/>
+                    <LikeButton isLiked={isLiked} handleLike={handleLike} />
                     <button className="action comment">{entrySvg.comment}</button>
                     <button className="action share">{entrySvg.share}</button>
                 </div>
                 <button className="action save">{entrySvg.save}</button>
             </div>
 
-            <div className="entry-likes">{likedBy.length} likes</div>
+            {likedBy.length !== 0 && (
+                <div className="entry-likes">
+                    {likedBy.length} like{likedBy.length === 1 ? '' : 's'}
+                </div>
+            )}
         </section>
     )
 }
