29f7cd55f9f7d212d9853133a454f7324613b270
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-18 22:54:14 +0200

    some fixes

--- src/cmps/CommentList.jsx
+++ src/cmps/CommentList.jsx
@@ -3,23 +3,11 @@ import { userService } from '../services/user'
 import { CommentPreview } from './CommentPreview.jsx'
 
 export function CommentList({ comments, onRemoveComment }) {
-    
-    function shouldShowActionBtns(comment) {
-        const user = userService.getLoggedinUser()
-        
-        if (!user) return false
-        if (user.isAdmin) return true
-        return comment.byUser?._id === user._id
-    }
-
     return <section>
-        <ul className="list comment-list">
+        <ul className="comment-list">
             {comments.map(comment =>
-                <li key={comment._id}>
+                <li key={comment.id}>
                     <CommentPreview comment={comment}/>
-                    {shouldShowActionBtns(comment) && <div className="actions">
-                        <button onClick={() => onRemoveComment(comment._id)}>x</button>
-                    </div>}
                 </li>)
             }
         </ul>
--- src/pages/CommentIndex.jsx
+++ src/pages/CommentIndex.jsx
@@ -1,54 +1,54 @@
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+// import { useEffect } from 'react'
+// import { useDispatch, useSelector } from 'react-redux'
 
-import { loadComments, removeComment, getActionAddComment, getActionRemoveComment } from '../store/actions/comment.actions'
-import { loadUsers } from '../store/actions/user.actions'
+// import { loadComments, removeComment, getActionAddComment, getActionRemoveComment } from '../store/actions/comment.actions'
+// import { loadUsers } from '../store/actions/user.actions'
 
-import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
-import { socketService, SOCKET_EVENT_COMMENT_ADDED, SOCKET_EVENT_COMMENT_REMOVED } from '../services/socket.service'
-import { CommentList } from '../cmps/CommentList'
-import { CommentEdit } from '../cmps/CommentEdit'
+// import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
+// import { socketService, SOCKET_EVENT_COMMENT_ADDED, SOCKET_EVENT_COMMENT_REMOVED } from '../services/socket.service'
+// import { CommentList } from '../cmps/CommentList'
+// import { CommentEdit } from '../cmps/CommentEdit'
 
-export function CommentIndex() {
-	const loggedInUser = useSelector(storeState => storeState.userModule.user)
-	const comments = useSelector(storeState => storeState.commentModule.comments)
+// export function CommentIndex() {
+// 	const loggedInUser = useSelector(storeState => storeState.userModule.user)
+// 	const comments = useSelector(storeState => storeState.commentModule.comments)
 
-	const dispatch = useDispatch()
+// 	const dispatch = useDispatch()
 
-	useEffect(() => {
-		loadComments()
-		loadUsers()
+// 	useEffect(() => {
+// 		loadComments()
+// 		loadUsers()
 
-		socketService.on(SOCKET_EVENT_COMMENT_ADDED, comment => {
-			console.log('GOT from socket', comment)
-			dispatch(getActionAddComment(comment))
-		})
+// 		socketService.on(SOCKET_EVENT_COMMENT_ADDED, comment => {
+// 			console.log('GOT from socket', comment)
+// 			dispatch(getActionAddComment(comment))
+// 		})
 
-		socketService.on(SOCKET_EVENT_COMMENT_REMOVED, commentId => {
-			console.log('GOT from socket', commentId)
-			dispatch(getActionRemoveComment(commentId))
-		})
+// 		socketService.on(SOCKET_EVENT_COMMENT_REMOVED, commentId => {
+// 			console.log('GOT from socket', commentId)
+// 			dispatch(getActionRemoveComment(commentId))
+// 		})
 
-		return () => {
-            socketService.off(SOCKET_EVENT_COMMENT_ADDED)
-            socketService.off(SOCKET_EVENT_COMMENT_REMOVED)
-        }
-	}, [])
+// 		return () => {
+//             socketService.off(SOCKET_EVENT_COMMENT_ADDED)
+//             socketService.off(SOCKET_EVENT_COMMENT_REMOVED)
+//         }
+// 	}, [])
 
-	async function onRemoveComment(commentId) {
-		try {
-			await removeComment(commentId)
-			showSuccessMsg('Comment removed')
-		} catch (err) {
-			showErrorMsg('Cannot remove')
-		}
-	}
+// 	async function onRemoveComment(commentId) {
+// 		try {
+// 			await removeComment(commentId)
+// 			showSuccessMsg('Comment removed')
+// 		} catch (err) {
+// 			showErrorMsg('Cannot remove')
+// 		}
+// 	}
 
-	return <div className="comment-index">
-        <h2>Comments and Gossip</h2>
-        {loggedInUser && <CommentEdit/>}
-        <CommentList 
-            comments={comments} 
-            onRemoveComment={onRemoveComment}/>
-    </div>
-}
+// 	return <div className="comment-index">
+//         <h2>Comments and Gossip</h2>
+//         {loggedInUser && <CommentEdit/>}
+//         <CommentList 
+//             comments={comments} 
+//             onRemoveComment={onRemoveComment}/>
+//     </div>
+// }
