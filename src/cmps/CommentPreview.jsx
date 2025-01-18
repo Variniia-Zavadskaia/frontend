966fd2bf8f3202d5143f32684081adae34d42e50
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-18 23:10:09 +0200

    add CommentPreview

--- src/cmps/CommentPreview.jsx
+++ src/cmps/CommentPreview.jsx
@@ -1,11 +1,36 @@
-import { Link } from 'react-router-dom'
+import { UserIcon } from './elements/UserIcon'
+import { UserName } from './elements/UserName'
+import { entrySvg } from './Svgs'
+import { getElapsedTime } from '../services/util.service'
+import { LikeButton } from './elements/LikeButton'
+import { useState } from 'react'
 
-export function CommentPreview({ comment }) {
-    const { byUser, aboutUser } = comment
+export function CommentPreview({ comment, isEntryMsg = false }) {
+    const userBy = comment.by
+    const [likedBy, setLikedBy] = useState(comment.likedBy ? [...comment.likedBy] : [])
+    const [isLiked, setIsLiked] = useState(false)
 
-    return <article className="preview comment-preview">
-        <p>About: <Link to={`/user/${aboutUser._id}`}>{aboutUser.fullname}</Link></p>
-        <p className="comment-by">By: <Link to={`/user/${byUser._id}`}>{byUser.fullname}</Link></p>
-        <p className="comment-txt">{comment.txt}</p>
-    </article>
+    function handleLike() {}
+
+    return (
+        <article className="comment-preview">
+            <UserIcon user={userBy} size={32} />
+            <div className="comment-body">
+                <div className="body-top" style={{ gap: `${isEntryMsg ? 8 : 4}px`}}>
+                    <UserName user={userBy} />
+                    {comment.date && <p>{getElapsedTime(comment.date)}</p>}
+                </div>
+                <p> {comment.txt} </p>
+                {!isEntryMsg && (
+                    <div className="body-bottom">
+                        {likedBy.length !== 0 && <button>
+                            {likedBy.length} like{likedBy.length === 1 ? '' : 's'}
+                        </button>}
+                        <button>{entrySvg.option}</button>
+                    </div>
+                )}
+            </div>
+            {!isEntryMsg && <LikeButton isLiked={isLiked} handleLike={handleLike} size={16}/>}
+        </article>
+    )
 }
