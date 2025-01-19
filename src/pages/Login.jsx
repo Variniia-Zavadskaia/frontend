6acb7b8e4f0ad543facb98f1baa1ac786dcc78fa
Variniia Zavadskaia <variniyaal@gmail.com>
2025-01-19 19:49:14 +0200

    fix login and signup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router'
+import { Link } from 'react-router-dom'
 
 import { userService } from '../services/user'
 import { login } from '../store/actions/user.actions'
+import { sideBarSvg } from '../cmps/Svgs'
 
 export function Login() {
     const [users, setUsers] = useState([])
@@ -32,17 +34,35 @@ export function Login() {
         const value = ev.target.value
         setCredentials({ ...credentials, [field]: value })
     }
-    
+
     return (
-        <form className="login-form" onSubmit={onLogin}>
-            <select
-                name="username"
-                value={credentials.username}
-                onChange={handleChange}>
-                    <option value="">Select User</option>
-                    {users.map(user => <option key={user._id} value={user.username}>{user.fullname}</option>)}
-            </select>
-            <button>Login</button>
-        </form>
+        <div className="login-container">
+            <div className="login-box">
+                <div className='logo'>{sideBarSvg.logo}</div>
+                <form className="login-form" onSubmit={onLogin}>
+                    <select name="username" value={credentials.username} onChange={handleChange}>
+                        <option value="">Select User</option>
+                        {users.map(user => (
+                            <option key={user._id} value={user.username}>
+                                {user.fullname}
+                            </option>
+                        ))}
+                    </select>
+                    <button type="submit" className="login-btn">
+                        Log in
+                    </button>
+                </form>
+                <div className="divider">
+                    <span>OR</span>
+                </div>
+                <button className="login-facebook-btn">Log in with Facebook</button>
+                <p className="forgot-password">Forgot password?</p>
+            </div>
+            <div className="signup-box">
+                <p>
+                    Don't have an account? <Link to="/signup">Sign up</Link>
+                </p>
+            </div>
+        </div>
     )
 }
