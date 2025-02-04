2bc0317cc0a912c9b51c5944b67883bffa3164b7
Variniia Zavadskaia <variniyaal@gmail.com>
2025-02-04 14:20:03 +0200

    widget suggestion starter

--- src/cmps/Widgets.jsx
+++ src/cmps/Widgets.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
@@ -12,55 +12,46 @@ export function Widgets() {
     const curUserImg = useSelector(storeState => storeState.userModule.user.imgUrl)
     const currUserName = useSelector(storeState => storeState.userModule.user.username)
     const currUserFull = useSelector(storeState => storeState.userModule.user.fullname)
+    const userFollowings = useSelector(storeState => storeState.userModule.user.following)
+    const allUsers = useSelector(storeState => storeState.userModule.users)
+    const [suggestions, setSuggestions] = useState(allUsers)
+
+    useEffect(() => {
+
+    }, [userFollowings])
+
+    function SuggestedUser() {
+        return (
+            <NavLink className="menu-item" to={`user/${currUserId}`}>
+                <img src="src/assets/icons/user.svg" alt="user Icon" className="icon regular" />
+                <div>
+                    <span className="text first">{currUserName}</span>
+                    <span className="text second">{currUserName}</span>
+                </div>
+                <button>Follow</button>
+            </NavLink>
+        )
+    }
 
     return (
         <section className="widgets">
-                <div className="menu-item">
-                    <UserIcon user={{ _id: currUserId, imgUrl: curUserImg }} size={44} />
-                    <div>
-                        <UserName className="text first" user={{ _id: currUserId, username: currUserName }} />
-                        <span className="text second">{currUserFull}</span>
-                    </div>
-                    <button>Switch</button>
+            <div className="menu-item">
+                <UserIcon user={{ _id: currUserId, imgUrl: curUserImg }} size={44} />
+                <div>
+                    <UserName className="text first" user={{ _id: currUserId, username: currUserName }} />
+                    <span className="text second">{currUserFull}</span>
                 </div>
+                <button>Switch</button>
+            </div>
             <div className="menu-item">
                 <span className="widg-txt">Suggested for you</span>
                 <button className="widg-txt-btn">See All</button>
             </div>
             <div>
-                <NavLink className="menu-item" to={`user/${currUserId}`}>
-                    <img src="src/assets/icons/user.svg" alt="user Icon" className="icon regular" />
-                    <div>
-                        <span className="text first">{currUserName}</span>
-                        <span className="text second">{currUserName}</span>
-                    </div>
-                    <button>Follow</button>
-                </NavLink>
-
-                <NavLink className="menu-item" to={`user/${currUserId}`}>
-                    <img src="src/assets/icons/user.svg" alt="user Icon" className="icon regular" />
-                    <div>
-                        <span className="text first">{currUserName}</span>
-                        <span className="text second">{currUserName}</span>
-                    </div>
-                    <button>Follow</button>
-                </NavLink>
-                <NavLink className="menu-item" to={`user/${currUserId}`}>
-                    <img src="src/assets/icons/user.svg" alt="user Icon" className="icon regular" />
-                    <div>
-                        <span className="text first">{currUserName}</span>
-                        <span className="text second">{currUserName}</span>
-                    </div>
-                    <button>Follow</button>
-                </NavLink>
-                <NavLink className="menu-item" to={`user/${currUserId}`}>
-                    <img src="src/assets/icons/user.svg" alt="user Icon" className="icon regular" />
-                    <div>
-                        <span className="text first">{currUserName}</span>
-                        <span className="text second">{currUserName}</span>
-                    </div>
-                    <button>Follow</button>
-                </NavLink>
+                <SuggestedUser />
+                <SuggestedUser />
+                <SuggestedUser />
+                <SuggestedUser />
             </div>
             <AppFooter />
         </section>
