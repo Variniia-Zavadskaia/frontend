de2972bded79d49335a8d4a3bca7ad884fac260b
Variniia Zavadskaia <variniyaal@gmail.com>
2025-02-08 18:20:03 +0700

    finish widgests

--- src/cmps/Widgets.jsx
+++ src/cmps/Widgets.jsx
@@ -4,24 +4,34 @@ import { useSelector } from 'react-redux'
 import { AppFooter } from './AppFooter'
 import { UserIcon } from './elements/UserIcon'
 import { UserName } from './elements/UserName'
-import { loadSuggestedUsers, loadUsers } from '../store/actions/user.actions'
+import { follow, loadSuggestedUsers, loadUsers } from '../store/actions/user.actions'
+import { showErrorMsg, showSuccessMsg } from '../services/event-bus.service'
 
 export function Widgets() {
     const currUserId = useSelector(storeState => storeState.userModule.user._id)
     const curUserImg = useSelector(storeState => storeState.userModule.user.imgUrl)
     const currUserName = useSelector(storeState => storeState.userModule.user.username)
     const currUserFull = useSelector(storeState => storeState.userModule.user.fullname)
+    const currUserFollowing = useSelector(storeState => storeState.userModule.user.following)
     const suggestedUsers = useSelector(storeState => storeState.userModule.suggestedUsers)
 
     useEffect(() => {
         loadSuggestedUsers(currUserId)
-        console.log('ggg')
-    }, [])
+    }, [currUserFollowing])
 
-    function SuggestedUser({idx}) {
+    async function onFollow(userId) {
+        try {
+            await follow(userId, true)
+            showSuccessMsg('Following successful')
+        } catch (err) {
+            showErrorMsg('Failed to follow')
+        }
+    }
+
+    function SuggestedUser({ idx }) {
         // console.log(suggestedUsers)
         if (!suggestedUsers || suggestedUsers.length === 0) return null
-        
+
         const suggestedUser = suggestedUsers[idx]
 
         return (
@@ -34,12 +44,10 @@ export function Widgets() {
                     />
                     <span className="text second">{suggestedUser.fullname}</span>
                 </div>
-                <button>Follow</button>
+                <button onClick={()=>{onFollow(suggestedUser._id)}}>Follow</button>
             </div>
         )
     }
-
-    console.log('hui')
 
     return (
         <section className="widgets">
@@ -55,9 +63,7 @@ export function Widgets() {
                 <span className="widg-txt">Suggested for you</span>
                 <button className="widg-txt-btn">See All</button>
             </div>
-            <div>
-                {suggestedUsers && suggestedUsers.map((user, idx) => <SuggestedUser key={user._id} idx={idx} />)}
-            </div>
+            <div>{suggestedUsers && suggestedUsers.map((user, idx) => <SuggestedUser key={user._id} idx={idx} />)}</div>
             <AppFooter />
         </section>
     )
