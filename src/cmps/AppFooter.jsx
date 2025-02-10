423107cf29f29c05cca28fffb315f45ecd0286ea
Variniia Zavadskaia <variniyaal@gmail.com>
2025-02-10 14:56:51 +0700

    fix app footer

--- src/cmps/AppFooter.jsx
+++ src/cmps/AppFooter.jsx
@@ -1,18 +1,22 @@
 import { Link, NavLink } from 'react-router-dom'
 
 export function AppFooter() {
+    console.log(process.env.NODE_ENV);
     
+
     return (
         <footer className="app-footer full">
-          
             <NavLink to="about">About</NavLink>
 
             <p>&copy; 2025 Shotverse from Variniia </p>
-
-            {import.meta.env.VITE_LOCAL ? (
-                <span className="local-services">Local Services</span>
-            ) : (
-                <span className="remote-services">Remote Services</span>
+            {process.env.NODE_ENV !== 'production' && (
+                <div>
+                    {import.meta.env.VITE_LOCAL ? (
+                        <span className="local-services">Local Services</span>
+                    ) : (
+                        <span className="remote-services">Remote Services</span>
+                    )}
+                </div>
             )}
         </footer>
     )
